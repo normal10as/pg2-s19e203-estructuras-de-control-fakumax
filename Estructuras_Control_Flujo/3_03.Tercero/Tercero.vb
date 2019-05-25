57b000e1b58913c23fcedcd6a89fc7dda761a73f
Facundo Vergara <fakumax@gmail.com>
2019-05-25 00:06:46 -0300

    V1.2

--- Estructuras_Control_Flujo/3_03.Tercero/Tercero.vb
+++ Estructuras_Control_Flujo/3_03.Tercero/Tercero.vb
@@ -1,30 +1,32 @@
 Imports System
-Imports System.Math
 '3.3. Crear un proyecto y un módulo “Tercero” para resolver con IF ingresar 5 valores enteros e
 'informar cual es el mayor por su posición de ingreso, por ejemplo, dado los siguientes
 'valores: 3,7,12,5,9 informar: “El tercer número es el mayor valor”.
 Module Tercero
     Sub Main(args As String())
-        Dim valor1, valor2, valor3, valor4, valor5 As Integer
-        Console.WriteLine("Ingrese un valor :")
-        valor1 = Console.ReadLine
-        Console.WriteLine("Ingrese un valor :")
-        valor2 = Console.ReadLine
-        Console.WriteLine("Ingrese un valor :")
-        valor3 = Console.ReadLine
-        Console.WriteLine("Ingrese un valor :")
-        valor4 = Console.ReadLine
-        Console.WriteLine("Ingrese un valor :")
-        valor5 = Console.ReadLine
-        Console.WriteLine("El número '{0}' está en la posición {1} : ", mayor(valor1, valor2, valor3, valor4, valor5))
+        Dim posicion As Integer = 1
+        Dim valorMayor As Integer
+        Dim valor1 As Integer = datoIngresado()
+        Dim valor2 As Integer = datoIngresado()
+        valorMayor = mayor(valor1, valor2, posicion)
+        Dim valor3 As Integer = datoIngresado()
+        valorMayor = mayor(valorMayor, valor3, posicion)
+        Dim valor4 As Integer = datoIngresado()
+        valorMayor = mayor(valorMayor, valor4, posicion)
+        Dim valor5 As Integer = datoIngresado()
+        valorMayor = mayor(valorMayor, valor5, posicion)
+        Console.WriteLine("El número mas grande es '{0}' en la posición '{1}'", valorMayor, posicion)
     End Sub
-    Function mayor(valor1 As Integer, valor2 As Integer, valor3 As Integer, valor4 As Integer, valor5 As Integer) As String
-        Dim aux As Integer = 1
-        If valor1 > valor2 Then
-            Max(valor1, valor2)
-        End If
-        Return ""
+    Function datoIngresado() As Integer
+        Console.WriteLine("Ingrese un valor :")
+        Return Console.ReadLine
     End Function
-
-
+    Function mayor(valor1 As Integer, valor2 As Integer, ByRef posicion As Integer) As Integer
+        If valor1 > valor2 Then
+            Return valor1
+        Else
+            posicion += 1
+            Return valor2
+        End If
+    End Function
 End Module
