f69b7fa45f75752c0ff81b0c7cd1b4355bbd792b
Facundo Vergara <fakumax@gmail.com>
2019-06-20 00:34:54 -0300

    V1.6

--- Estructuras_Control_Flujo/3_13.Primos/Primos.vb
+++ Estructuras_Control_Flujo/3_13.Primos/Primos.vb
@@ -4,6 +4,22 @@ Imports System
 'únicamente dos divisores distintos: él mismo y el 1.
 Module Primos
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Los Numeroros Primos son : " & vbNewLine)
+        numerosPrimos(1000)
     End Sub
+    Sub numerosPrimos(maxNumero As UInt32)
+        Dim primos As Integer
+        For x As Integer = 2 To maxNumero
+            For n = 1 To x
+                If x Mod n = 0 Then
+                    primos += 1
+                End If
+            Next
+            If primos = 2 Then
+                Console.Write(x & ",")
+            End If
+            primos = 0
+        Next
+    End Sub
+
 End Module
